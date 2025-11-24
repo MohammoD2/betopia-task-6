006de5a0b688041bcf9c7480cf459ebf3c16d466
MohammoD2 <ibr500037@gmail.com>
2025-11-24 15:43:20 +0600

    Website Conversation AI

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 OPENROUTER_MODEL = "meta-llama/llama-3.3-70b-instruct:free"
 
 # ---------------------------
